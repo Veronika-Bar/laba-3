8ed2378b1984b1627a5c1cadef8e58c5e99734df
Vera <barinova2005@inbox.ru>
2024-10-08 09:57:53 +0300

    prog

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -1,7 +1,17 @@
 package main
-
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+ var input string
+ fmt.Scan(&input)
+    
+ maxDigit := '0' 
+
+ for _, char := range input {
+  if char > maxDigit {
+   maxDigit = char 
+  }
+ }
+
+ fmt.Printf("%c\n", maxDigit)
 }
