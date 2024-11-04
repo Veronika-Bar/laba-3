b3adce04f183e571e221743621adaee0f70805fe
Vera <barinova2005@inbox.ru>
2024-11-04 17:03:27 +0300

    prog

--- projects/math/main.go
+++ projects/math/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+var k, p, v float64 = 1296, 6, 6
+
+// Функция для вычисления массы
+func M() float64 {
+	return p * v
+}
+
+// Функция для вычисления циклической частоты
+func W() float64 {
+	m := M()
+	return math.Sqrt(k / m)
+}
+
+// Функция для вычисления периода колебаний
+func T() float64 {
+	w := W()
+	return 6 / w
+}
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	// Пример использования функции T()
+	period := T()
+	fmt.Println("Период колебаний (t):", period) //1
 }
